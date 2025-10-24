de5186a1e564c50dd02932f0648a93fd316744bb
daobt <buidao1703@gmail.com>
2025-10-24 10:36:26 +0700

    test app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo/packages/chat/pay/lib/main.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +12,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          children: [
+            ChatPayScreen()
+          ],
         ),
       ),
     );
